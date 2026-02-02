bf0c164d6ddf78400f3fa5801970ba7cb44e7d85
Maxime Ducamp <maxime.ducamp@gmail.com>
2026-02-02 12:44:42 +0100

    fix index on email and userName that warn on seed script from ubuntu server

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -233,8 +233,6 @@ userSchema.set("toJSON", { virtuals: true });
 userSchema.set("toObject", { virtuals: true });
 
 // Indexes for efficient queries
-userSchema.index({ email: 1 }, { unique: true });
-userSchema.index({ userName: 1 }, { unique: true, sparse: true });
 userSchema.index({ artistStatus: 1 });
 userSchema.index({ role: 1 });
 
